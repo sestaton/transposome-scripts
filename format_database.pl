7daf22970d60e2b8d14616bac9526bf5fae43238
sestaton <statonse@gmail.com>
2014-11-28 17:14:41 -0500

    update listing

--- format_database.pl
+++ format_database.pl
@@ -20,7 +20,7 @@ GetOptions(
            'g|genus=s'   => \$genus,
            's|species=s' => \$species,
            'h|help'      => \$help,
-    );
+          );
 
 usage() and exit(0) if $help;
 
@@ -100,7 +100,7 @@ USAGE: $script [-i] [-o] [-g] [-s] [-h]
 
 Required:
 -i|infile              :       A Fasta file of repeats to be formatted for Transposome.
--o|outfile             :       A name for the formatted database..
+-o|outfile             :       A name for the formatted database.
 -g|genus               :       The source genus of the sequences.
 -s|species             :       The source species of the sequences.
 
