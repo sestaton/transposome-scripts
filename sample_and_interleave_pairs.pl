2a57f1eb44593f8c70eee3e62d8ecac297f07efb
sestaton <statonse@gmail.com>
2015-01-03 12:42:02 -0800

    remove long lines and fix menu

--- sample_and_interleave_pairs.pl
+++ sample_and_interleave_pairs.pl
@@ -30,8 +30,18 @@ usage() and exit(1) if !$pair1 || !$pair2 || !$sample_size || !$outfile;
 $seq_format //= 'fasta';
 open my $out, '>', $outfile;
 
-my $sequtil_pair1 = Transposome::SeqUtil->new( file => $pair1, format => $seq_format, sample_size => $sample_size, seed => 100, in_memory => 1 );
-my $sequtil_pair2 = Transposome::SeqUtil->new( file => $pair2, format => $seq_format, sample_size => $sample_size, seed => 100, in_memory => 1 );
+my $sequtil_pair1 = Transposome::SeqUtil->new( file        => $pair1, 
+					       format      => $seq_format, 
+                                               sample_size => $sample_size, 
+                                               seed        => 100, 
+                                               in_memory   => 1 );
+
+my $sequtil_pair2 = Transposome::SeqUtil->new( file        => $pair2, 
+                                               format      => $seq_format, 
+                                               sample_size => $sample_size, 
+                                               seed        => 100, 
+                                               in_memory   => 1 );
+
 my ($seqs1, $seqs1_ct) = $sequtil_pair1->sample_seq;
 my ($seqs2, $seqs2_ct) = $sequtil_pair2->sample_seq;
 
@@ -55,14 +65,14 @@ sub usage {
 USAGE: $script [-f] [-r] [-o] [-n] [-h]
 
 Required:
--f|pair2           :       The forwared sequence file to sample.
--r|pair2           :       The reverse sequence file to sample.
--o|outfile         :       The number of sequences to sample (integer).
--n|sample_size     :       (NB: the value may be given as '1000000' or with underscores
-                            as in '1_000_000').
+ -f|pair2           :       The forwared sequence file to sample.
+ -r|pair2           :       The reverse sequence file to sample.
+ -o|outfile         :       The number of sequences to sample (integer).
+ -n|sample_size     :       (NB: the value may be given as '1000000' or with underscores
+                             as in '1_000_000').
 
 Options:
--h|help            :       Print a usage statement.
+ -h|help            :       Print a usage statement.
 
 END
 }
