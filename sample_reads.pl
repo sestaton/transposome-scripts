8e7ca09ed4f42b29788c0c6a9cd60f08569ee928
sestaton <statonse@gmail.com>
2015-07-08 11:22:04 -0700

    remove alternate version number bc of type constraint issue

--- sample_reads.pl
+++ sample_reads.pl
@@ -51,8 +51,7 @@ USAGE: $script -i seqs.fasta -n 100000
 Required:
  -i|infile           :       The sequence file to sample.
  -n|sample_size      :       The number of sequences to sample (integer).
-                             (NB: the value may be as '1000000' or with underscores
-                              as in '1_000_000').
+
 Options:
  -f|format            :       The input sequence format (Default: FASTA).
  -h|help              :       Print a usage statement.
