6fc58d544cc91ee0a4acd27c483c1b82ef9597a6
sestaton <statonse@gmail.com>
2017-05-31 13:20:28 -0700

    update formatting script to handle repeatmodeler format

--- format_database.pl
+++ format_database.pl
@@ -4,7 +4,9 @@ use 5.010;
 use strict;
 use warnings;
 use autodie qw(open);
+use Cwd     qw(getcwd);
 use File::Basename;
+use Transposome::Annotation;
 use Transposome::SeqFactory;
 use Getopt::Long;
 
@@ -12,6 +14,7 @@ my $infile;
 my $outfile; 
 my $genus;
 my $species;
+my $quiet;
 my $help;
 
 GetOptions(
@@ -19,6 +22,7 @@ GetOptions(
            'o|outfile=s' => \$outfile,
            'g|genus=s'   => \$genus,
            's|species=s' => \$species,
+           'q|quiet'     => \$quiet,
            'h|help'      => \$help,
           );
 
@@ -30,7 +34,19 @@ if (!$infile || !$outfile || !$genus || !$species) {
     exit(1);
 }
 
+my $outdir  = getcwd();
 my $epithet = $genus.q{ }.$species;
+my $report  = 'tmp_transposome_format_rep.txt';
+
+# these class attributes are being faked because we just need one method
+my $annotation = Transposome::Annotation->new(
+      database => $infile, 
+      dir      => $outdir,
+      file     => $report,
+      threads  => 1,
+      cpus     => 1,
+      verbose  => 0,
+);
 
 open my $out, '>', $outfile;
 my $seqio = Transposome::SeqFactory->new( file => $infile )->make_seqio_object;
@@ -44,50 +60,24 @@ while (my $seq = $seqio->next_seq) {
     my $nt = $seq->get_seq;
     $nt =~ s/.{60}\K/\n/g;
 
-    if ($id =~ /^RLG/) {
-        say $out join "\n", ">".$id."\t"."Gypsy"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^RLC/) {
-        say $out join "\n", ">".$id."\t"."Copia"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^DHH/) {
-        say $out join "\n", ">".$id."\t"."Helitron"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^DTA/) {
-        say $out join "\n", ">".$id."\t"."hAT"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^DTC/) {
-        say $out join "\n", ">".$id."\t"."CACTA"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^DTH/) {
-        say $out join "\n", ">".$id."\t"."PIF/Harbinger"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^DTM/) {
-        say $out join "\n", ">".$id."\t"."Mutator"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^DTT/) {
-        say $out join "\n", ">".$id."\t"."Tc1/Mariner"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^PPP/) {
-        say $out join "\n", ">".$id."\t"."Penelope"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^RIL/) {
-        say $out join "\n", ">".$id."\t"."L1"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^RIT/) {
-        say $out join "\n", ">".$id."\t"."RTE"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^RLX/) {
-        say $out join "\n", ">".$id."\t"."Unknown_LTR"."\t".$epithet, $nt;
-    }
-    elsif ($id =~ /^RST/) {
-        say $out join "\n", ">".$id."\t"."SINE2/tRNA"."\t".$epithet, $nt;
+    my $superfamily = $annotation->map_superfamily_name($id);
+
+    if ($superfamily) { 
+	say $out join "\n", ">".$id."\t".$superfamily."\t".$epithet, $nt;
     }
     else {
-        # should never get here, but the data may be malformed
-        say STDERR "\n[ERROR]: ",$seq->get_id," does not seem to match known TE superfamilies";
+	if ($id =~ /#(\w+)\/?(\w+(\d+)?)?$/) {
+	    my $name = defined $2 ? $2 : $1;
+	    say $out join "\n", ">".$id."\t".$name."\t".$epithet, $nt;
+	    next;
+	}
+
+	say STDERR "\n[WARNING]: ",$seq->get_id," does not seem to match known TE superfamilies\n"
+	    unless $quiet;
+	say $out join "\n", ">".$id."\tUnknown_repeat\t".$epithet, $nt;
     }
 }
+
 close $out;
 
 exit;
@@ -105,6 +95,9 @@ Required:
  -s|species             :       The source species of the sequences.
 
 Options:
+ -q|quiet               :       Silence warnings when known superfamilies cannot be matched. Note
+                                that some warnings will still come through with regards to the
+                                ID format, but these are just warnings and can be ignored.
  -h|help                :       Print a usage statement.
 
 END
